652d1b1b4585d2c7bbd616cf38d0564aad68383f
cyberpanknout <cyber-pank@yandex.ru>
2026-02-28 16:53:55 +0300

    1.0.1

---
+++ src/Glava_1/MyFirstApp.java
@@ -0,0 +1,9 @@
+package Glava_1;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Весь мир!");
+        System.out.println("В моих руках");
+    }
+}
+
