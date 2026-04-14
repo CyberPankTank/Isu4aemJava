da78f9415cd518f97b6778e6fe2b41370ff4256d
cyberpanknout <cyber-pank@yandex.ru>
2026-04-14 22:04:54 +0300

    13.0.1

---
+++ src/Glava_13/MusicTest1.java
@@ -0,0 +1,16 @@
+package Glava_13;
+import javax.sound.midi.*;
+public class MusicTest1 {
+    public void play() {
+        try {
+            Sequence sequencer = MidiSystem.getSequencer();
+            System.out.println("Суквенсор успешно установлен");
+        } catch (MidiUnavailableException e) {
+            System.out.println("Облом");
+        }
+    }
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
