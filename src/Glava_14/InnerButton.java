5ca57d5c7898f1c6beadac20c780a411b06b52ef
cyberpanknout <cyber-pank@yandex.ru>
2026-04-26 20:56:55 +0300

    14.0.final

---
+++ src/Glava_14/InnerButton.java
@@ -0,0 +1,34 @@
+package Glava_14;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class InnerButton {
+    private JButton button;
+
+    public static void main() {
+        InnerButton gui = new InnerButton();
+        gui.go();
+    }
+    public void go() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        button = new JButton("A");
+        button.addActionListener(new ButtonListener());
+
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setSize(200, 100);
+        frame.setVisible(true);
+    }
+    class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (button.getText().equals("A")) {
+                button.setText("B");
+            } else {
+                button.setText("A");
+            }
+        }
+    }
+}
