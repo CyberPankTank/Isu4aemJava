9afcf9e90c099a6a533188cce8f9b2d39cb4fdae
cyberpanknout <cyber-pank@yandex.ru>
2026-04-26 23:31:17 +0300

    15.0.1

---
+++ src/Glava_15/TextArea1.java
@@ -0,0 +1,28 @@
+package Glava_15;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TextArea1 {
+    public static void main(String[] args) {
+        TextArea1 gui = new TextArea1();
+        gui.go();
+    }
+    public void go() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Кликни");
+        JTextArea text = new JTextArea(10, 20);
+        text.setLineWrap(true);
+        button.addActionListener(e -> text.append("Кнопка нажата \n"));
+        JScrollPane scroller = new JScrollPane(text);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(scroller);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(350, 300);
+        frame.setVisible(true);
+    }
+}
