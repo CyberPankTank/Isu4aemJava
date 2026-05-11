bec3c40567b06494e28e1368503fa9eb4a41138e
cyberpanknout <cyber-pank@yandex.ru>
2026-05-11 12:21:33 +0300

    18.0.final

---
+++ src/Glava_18/TwoThreadsWritihg.java
@@ -0,0 +1,35 @@
+package Glava_18;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TwoThreadsWritihg {
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        Data data = new Data();
+        threadPool.execute(() -> addLetterToDay('a', data));
+        threadPool.execute(() -> addLetterToDay('A', data));
+        threadPool.shutdown();
+    }
+    private static void addLetterToDay(char letter, Data data) {
+        for (int i = 0; i < 26; i++) {
+            data.addLetter(letter++);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ignored) {}
+        }
+        System.out.println(Thread.currentThread().getName() + data.getLetters());
+        System.out.println(Thread.currentThread().getName() + " size = " + data.getLetters().size());
+    }
+}
+final class Data {
+    private final List<String> letters = new ArrayList<>();
+
+    public List<String> getLetters() {
+        return letters;
+    }
+    public synchronized void addLetter(char letter) {
+        letters.add(String.valueOf(letter));
+    }
+}
